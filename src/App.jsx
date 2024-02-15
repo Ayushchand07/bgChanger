b9d62aa3fcba84bdd6a2e01b1bda6a69dae9ac1d
Ayushchand07 <140510300+Ayushchand07@users.noreply.github.com>
2024-02-15 18:50:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,13 +17,13 @@ function App() {
         <div className='fixed flex flexwrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
           <button
           onClick={() => setColor('red')}
-          className='outline-none px-4 py-1 rounded-dull shadow-lg text-red-500 '>laal main choos le </button>
+          className='outline-none px-4 py-1 rounded-dull shadow-lg text-red-500 '>Red </button>
           <button 
           onClick={() => setColor('blue')}
-          className='outline-none px-4 py-1 rounded-dull shadow-lg text-blue-500'>neele main choos le </button>
+          className='outline-none px-4 py-1 rounded-dull shadow-lg text-blue-500'>Blue </button>
           <button 
           onClick={() => setColor('green')}
-          className='outline-none px-4 py-1 rounded-dull shadow-lg text-green-500'>haare main choos le</button>
+          className='outline-none px-4 py-1 rounded-dull shadow-lg text-green-500'>Green</button>
 
         </div>
 
